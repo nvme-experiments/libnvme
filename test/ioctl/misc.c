c62815f4e4be25f10a61260e6a14e39e951f0e0e
Tokunori Ikegami <ikegami.t@gmail.com>
2025-08-11 11:07:00 +0900

    ioctl: nvme_io_mgmt_recv use nvme_passthru_cmd directly
    Drop struct nvme_io_mgmt_recv_args.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- test/ioctl/misc.c
+++ test/ioctl/misc.c
@@ -1218,29 +1218,23 @@ static void test_resv_report(void)
 static void test_io_mgmt_recv(void)
 {
 	__u8 expected_data[8], data[8] = {};
-	struct nvme_io_mgmt_recv_args args = {
-		.data = &data,
-		.args_size = sizeof(args),
-		.nsid = TEST_NSID,
-		.data_len = sizeof(data),
-		.mos = 0x1,
-		.mo = 0x2,
-	};
+	__u32 data_len = sizeof(data);
+	__u16 mos = 0x1;
+	__u8 mo = 0x2;
+	int err;
 
 	struct mock_cmd mock_io_cmd = {
 		.opcode = nvme_cmd_io_mgmt_recv,
 		.nsid = TEST_NSID,
-		.cdw10 = args.mo | (args.mos << 16),
-		.cdw11 = (args.data_len >> 2) - 1,
-		.data_len = args.data_len,
+		.cdw10 = mo | (mos << 16),
+		.cdw11 = (data_len >> 2) - 1,
+		.data_len = data_len,
 		.out_data = &expected_data,
 	};
 
-	int err;
-
 	arbitrary(&expected_data, sizeof(expected_data));
 	set_mock_io_cmds(&mock_io_cmd, 1);
-	err = nvme_io_mgmt_recv(test_link, &args);
+	err = nvme_io_mgmt_recv(test_link, TEST_NSID, mos, mo, &data, data_len);
 	end_mock_cmds();
 	check(err == 0, "returned error %d", err);
 	cmp(&data, &expected_data, sizeof(data), "incorrect data");
