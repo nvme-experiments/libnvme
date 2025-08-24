94b59f09670c484a2ec8afeeaa71e4c3fc420c68
Tokunori Ikegami <ikegami.t@gmail.com>
2025-08-25 00:11:46 +0900

    ioctl: nvme_uring_cmd_admin_passthru_async use nvme_passthru_cmd
    Since already struct nvme_get_log_args dropped.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- src/nvme/ioctl.c
+++ src/nvme/ioctl.c
@@ -336,50 +336,21 @@ static void nvme_uring_cmd_exit(struct io_uring *ring)
 }
 
 static int nvme_uring_cmd_admin_passthru_async(nvme_link_t l, struct io_uring *ring,
-					       struct nvme_get_log_args *args)
+					       struct nvme_passthru_cmd *cmd, __u32 *result)
 {
 	struct io_uring_sqe *sqe;
-	struct nvme_uring_cmd *cmd;
 	int ret;
-
-	__u32 numd = (args->len >> 2) - 1;
-	__u16 numdu = numd >> 16, numdl = numd & 0xffff;
-
-	__u32 cdw10 = NVME_SET(args->lid, LOG_CDW10_LID) |
-			NVME_SET(args->lsp, LOG_CDW10_LSP) |
-			NVME_SET(!!args->rae, LOG_CDW10_RAE) |
-			NVME_SET(numdl, LOG_CDW10_NUMDL);
-	__u32 cdw11 = NVME_SET(numdu, LOG_CDW11_NUMDU) |
-			NVME_SET(args->lsi, LOG_CDW11_LSI);
-	__u32 cdw12 = args->lpo & 0xffffffff;
-	__u32 cdw13 = args->lpo >> 32;
-	__u32 cdw14 = NVME_SET(args->uuidx, LOG_CDW14_UUID) |
-			NVME_SET(!!args->ot, LOG_CDW14_OT) |
-			NVME_SET(args->csi, LOG_CDW14_CSI);
-
-	if (args->args_size < sizeof(struct nvme_get_log_args))
-		return -EINVAL;
 
 	sqe = io_uring_get_sqe(ring);
 	if (!sqe)
 		return -1;
 
-	cmd = (void *)&sqe->cmd;
-	cmd->opcode        = nvme_admin_get_log_page,
-	cmd->nsid          = args->nsid,
-	cmd->addr          = (__u64)(uintptr_t)args->log,
-	cmd->data_len      = args->len,
-	cmd->cdw10         = cdw10,
-	cmd->cdw11         = cdw11,
-	cmd->cdw12         = cdw12,
-	cmd->cdw13         = cdw13,
-	cmd->cdw14         = cdw14,
-	cmd->timeout_ms    = args->timeout,
+	memcpy(&sqe->cmd, cmd, sizeof(*cmd));
 
 	sqe->fd = l->fd;
 	sqe->opcode = IORING_OP_URING_CMD;
 	sqe->cmd_op = NVME_URING_CMD_ADMIN;
-	sqe->user_data = (__u64)(uintptr_t)args;
+	sqe->user_data = (__u64)(uintptr_t)result;
 
 	ret = io_uring_submit(ring);
 	if (ret < 0)
@@ -390,9 +361,9 @@ static int nvme_uring_cmd_admin_passthru_async(nvme_link_t l, struct io_uring *r
 
 static int nvme_uring_cmd_wait_complete(struct io_uring *ring, int n)
 {
-	struct nvme_get_log_args *args;
 	struct io_uring_cqe *cqe;
 	int i, ret = 0;
+	__u32 *result;
 
 	for (i = 0; i < n; i++) {
 		ret = io_uring_wait_cqe(ring, &cqe);
@@ -400,9 +371,9 @@ static int nvme_uring_cmd_wait_complete(struct io_uring *ring, int n)
 			return -1;
 
 		if (cqe->res) {
-			args = (struct nvme_get_log_args *)cqe->user_data;
-			if (args->result)
-				*args->result = cqe->res;
+			result = (__u32 *)cqe->user_data;
+			if (result)
+				*result = cqe->res;
 			ret = cqe->res;
 			break;
 		}
@@ -489,7 +460,7 @@ int nvme_get_log_partial(nvme_link_t l, struct nvme_passthru_cmd *cmd,
 				n = 0;
 			}
 			n += 1;
-			ret = nvme_uring_cmd_admin_passthru_async(l, &ring, args);
+			ret = nvme_uring_cmd_admin_passthru_async(l, &ring, cmd, result);
 
 			if (ret)
 				nvme_uring_cmd_exit(&ring);
